d424cd8f5be7622bfc242d8fdaba86b88ccb8ced
Ryan Tang <ryantang@gmail.com>
2025-07-22 16:27:36 -0700

    Add infrastructure for frontend testing
    Also, cleaned up some unused packages like replit.

---
+++ client/src/test/setup.ts
@@ -0,0 +1,14 @@
+import '@testing-library/jest-dom'
+
+// Global test configuration
+Object.defineProperty(window, 'matchMedia', {
+  writable: true,
+  value: (query: string) => ({
+    matches: false,
+    media: query,
+    onchange: null,
+    addEventListener: () => {},
+    removeEventListener: () => {},
+    dispatchEvent: () => {},
+  }),
+})
